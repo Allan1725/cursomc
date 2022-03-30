65114d55e6a7b1ad1068d7020f0fa4095e35e4fe
Allan1725 <allansantos.3949@gmail.com>
2022-03-29 21:39:30 -0300

    Rest Testado

--- src/main/java/com/allansantos/cursomc/CursomcApplication.java
+++ src/main/java/com/allansantos/cursomc/CursomcApplication.java
@@ -1,4 +1,4 @@
-package com.allansantos;
+package com.allansantos.cursomc;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
